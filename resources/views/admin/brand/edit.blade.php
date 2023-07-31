5ee348e39ad0fb235390ce1d78b807b8e732b42c
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-07-31 08:49:40 +0200

    Setup home page

--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -1,10 +1,6 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Edit Brands
-        </h2>
-    </x-slot>
+@extends('admin.admin_master')
 
+@section('admin')
     @if (session('success'))
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 <strong>{{ session('success') }}</strong>
@@ -57,4 +53,4 @@
             </div>    
         </div>
     </div>
-</x-app-layout>
+@endsection
